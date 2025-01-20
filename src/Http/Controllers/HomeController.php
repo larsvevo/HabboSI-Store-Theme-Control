3e6093fdfd40d885db9d0c1d33473304903e4023
larsvevo <ardaadinc2003@gmail.com>
2025-01-20 12:49:36 +0300

    Update HomeController.php

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace Atom\Theme\Http\Controllers;
 
-use Atom\Core\Models\CameraWeb;
-use Atom\Core\Models\WebsiteArticle;
+use Atom\Core\Models\WebsiteShopArticle;
+use Atom\Core\Models\WebsiteShopCategory;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\View\View;
-use Atom\Core\Models\Room;
-use Atom\Core\Models\Guild;
-use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
 
 class HomeController extends Controller
 {
